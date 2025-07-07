1f25fc9fc50c73c5fb59e64e33cb93608880d35b
surya charan reddy <suryacharan1861@gmail.com>
2025-07-07 10:07:35 +0530

    07/07 update

---
+++ Javascript/for-loop.js
@@ -0,0 +1,13 @@
+//initilization st:i=1;
+//condition     st:i<=20;
+//increment     st:i++;
+for(let i=1; i<=20 ;i++){
+    if (i%2===0){
+        console.log(i)
+    }
+}
+
+for(n=100 ; n>=51 ;n--)
+{
+    console.log(n)
+}
---
+++ Javascript/if-else.js
@@ -0,0 +1,26 @@
+let sal=22000;
+if(sal>=40000){
+    console.log("You are eligible for the loan");
+}
+else{
+    console.log("Your Not eligible for the loan")
+}
+//it is iterative of statement can write a cod ein a single line
+var esal=50000
+esal>=40000 ? console.log("eligible") : console.log("not eligible")
+
+let num=27
+if(num%2===0){
+    console.log("The given number is Even")
+}
+else{
+    console.log("The given number is odd")
+}
+
+let numb=223
+if (numb>=100 && numb<=999){
+    console.log("Given number is  three digit number")
+}
+else{
+    console.log("Given number is not a Three digit")
+}
