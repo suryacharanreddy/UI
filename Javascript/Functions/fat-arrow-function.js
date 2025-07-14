a9f16e02483b9c3808ef838ebb13849be7772f00
surya charan reddy <suryacharan1861@gmail.com>
2025-07-14 09:55:21 +0530

    14/07 update

---
+++ Javascript/Functions/fat-arrow-function.js
@@ -0,0 +1,20 @@
+/* function add(a,b){
+    console.log(a+b);
+}
+add(10,20)
+//writing a function and converting into a fat arrow function.
+let addi=(b,c)=>{
+    console.log(b*c);
+}
+addi(2,3)
+ */
+let ad=(a,b)=>a+b;
+
+let r1=ad(1,2);
+console.log(r1)
+
+let r2=ad('surya',4)
+console.log(r2)
+
+let r3=ad(1);
+console.log(r3)
